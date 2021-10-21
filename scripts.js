d19505260a024443709d2a6b385a7fd9770ea6b8
Shantel Wellington <shantelwellington03@gmail.com>
2021-10-21 01:56:59 -0700

    js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
 /* Add your JavaScript to this file */
+
+window.onload = function()
+{
+    buttons = document.getElementsByClassName("btn"); /*gets all the button classes */
+    formButton = buttons[1];
+
+    formButton.addEventListener("click", clickListner);
+    email =  document.getElementById("email");
+
+    message = document.getElementsByClassName("message")[0];
+    console.log(message);
+
+    function clickListner(e){
+        e.preventDefault();
+        if(email.value != ""){
+            message.innerHTML = "Thank you! your email address " + email.value + " has been added to our mailing list" ;
+            email.value = "";
+        }
+        else{
+            message.innerHTML = "Please enter a valid email address";
+            email.value = "";
+        }
+    }
+
+}
